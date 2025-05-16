eed22ceece60b813198dd7c8e8fcd521840df68d
KimSeungBin <77220945+LaverWinEmpty@users.noreply.github.com>
2025-05-16 17:26:25 +0900

    feat: hash

---
+++ source/util/hash.hpp
@@ -0,0 +1,38 @@
+#ifndef LWE_UTIL_HASH
+#define LWE_UTIL_HASH
+
+#include "../common.hpp"
+
+LWE_BEGIN
+namespace util {
+
+    class Hash {
+        static constexpr unsigned long long FNV1A64_BASIS = 14695981039346656037ULL;
+        static constexpr unsigned long long FNV1A64_PRIME = 1099511628211ULL;
+    
+    public:
+        Hash(const void*, size_t);
+        Hash(const char*);
+        Hash(const string&);
+        template<typename T> Hash(const T&);
+    
+    public:
+        bool operator==(const Hash&) const;
+        bool operator!=(const Hash&) const;
+    
+    public:
+        const char* operator*() const; //!< get string (c_str)
+    
+    public:
+        operator hash_t() const; //!< get value
+    
+    private:
+        hash_t val     = 0;
+        char   str[17] = { 0 };
+    };
+
+}
+LWE_END
+
+#include "hash.ipp"
+#endif
---
+++ source/util/hash.ipp
@@ -0,0 +1,38 @@
+#ifdef LWE_UTIL_HASH
+
+LWE_BEGIN
+namespace util {
+
+Hash::Hash(const void* in, size_t n): val(FNV1A64_BASIS) {
+    const unsigned char* ptr = static_cast<const unsigned char*>(in);
+    for (size_t i = 0; i < n; i++) {
+        val ^= ptr[i];
+        val *= FNV1A64_PRIME;
+    }
+    std::format_to_n(str, sizeof(str) - 1, "{:X}", val);
+}
+
+Hash::Hash(const string& in): Hash(in.c_str(), in.size()) {}
+
+bool Hash::operator==(const Hash& in) const {
+    return val == in.val;
+}
+
+bool Hash::operator!=(const Hash& in) const {
+    return val != in.val;
+}
+
+Hash::Hash(const char* in) : Hash(in, std::strlen(in)) {}
+
+template<typename T> Hash::Hash(const T& in): Hash(&in, sizeof(T)) {}
+
+const char* Hash::operator*() const {
+    return str;
+}
+
+Hash::operator hash_t() const {
+    return val;
+}
+
+}
+LWE_END
